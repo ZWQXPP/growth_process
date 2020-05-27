7e808e75a6fe67cd493a8f07d80512f2202e97a6
zhangwanqi <zhangwanqi@sdxnetcafe.com>
2020-05-27 20:15:57 +0800

    growing_process

--- ly_item/ly_item_service/src/main/java/com/leyou/item/biz/ActivitySolidAwardYardBiz.java
+++ ly_item/ly_item_service/src/main/java/com/leyou/item/biz/ActivitySolidAwardYardBiz.java
@@ -17,7 +17,7 @@ import java.util.Date;
 public class ActivitySolidAwardYardBiz {
 
     @Autowired
-    private ActivitySolidAwardYardMapper awardYardMapper;
+    private  ActivitySolidAwardYardMapper awardYardMapper;
 
         public void addYard(ActivitySolidAwardYardVo activitySolidAwardYardVo) {
         checkAwardYard(activitySolidAwardYardVo);
