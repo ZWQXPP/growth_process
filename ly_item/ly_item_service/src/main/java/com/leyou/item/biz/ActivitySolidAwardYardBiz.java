413f892891aad2ac56575fae268b26e476b58ebc
zhangwanqi <zhangwanqi@sdxnetcafe.com>
2020-05-28 09:49:24 +0800

    测试4

--- ly_item/ly_item_service/src/main/java/com/leyou/item/biz/ActivitySolidAwardYardBiz.java
+++ ly_item/ly_item_service/src/main/java/com/leyou/item/biz/ActivitySolidAwardYardBiz.java
@@ -17,48 +17,44 @@ import java.util.Date;
 public class ActivitySolidAwardYardBiz {
 
     @Autowired
-    private  ActivitySolidAwardYardMapper awardYardMapper;
+    private ActivitySolidAwardYardMapper awardYardMapper;
 
-        public void addYard(ActivitySolidAwardYardVo activitySolidAwardYardVo) {
+    public void addYard(ActivitySolidAwardYardVo activitySolidAwardYardVo) {
         checkAwardYard(activitySolidAwardYardVo);
-            ActivitySolidAwardYard activitySolidAwardYard = new ActivitySolidAwardYard();
-            activitySolidAwardYardVo.setDeleted(false);
-            activitySolidAwardYardVo.setUpdatedTime(new Date());
-            activitySolidAwardYardVo.setCreatedTime(new Date());
-            BeanUtils.copyProperties(activitySolidAwardYardVo,activitySolidAwardYard);
-            if (activitySolidAwardYard == null){
-                awardYardMapper.insert(activitySolidAwardYard);
-            }else {
-                awardYardMapper.updateByPrimaryKeySelective( activitySolidAwardYard);
-            }
-
-
+        ActivitySolidAwardYard activitySolidAwardYard = new ActivitySolidAwardYard();
+        activitySolidAwardYardVo.setDeleted(false);
+        activitySolidAwardYardVo.setUpdatedTime(new Date());
+        activitySolidAwardYardVo.setCreatedTime(new Date());
+        BeanUtils.copyProperties(activitySolidAwardYardVo, activitySolidAwardYard);
+        if (activitySolidAwardYard == null) {
+            awardYardMapper.insert(activitySolidAwardYard);
+        } else {
+            awardYardMapper.updateByPrimaryKeySelective(activitySolidAwardYard);
+        }
     }
 
     private void checkAwardYard(ActivitySolidAwardYardVo activitySolidAwardYardVo) {
-            //详情
+        //详情
         ActivitySolidAwardYard activitySolidAwardYard = awardYardMapper.selectByPrimaryKey(activitySolidAwardYardVo.getId());
         //判断验证码是否为空
-        if (activitySolidAwardYard == null){
-            if (activitySolidAwardYardVo.getActivitySolidAwardId()==null){
+        if (activitySolidAwardYard == null) {
+            if (activitySolidAwardYardVo.getActivitySolidAwardId() == null) {
                 throw new ServiceException("实体奖品ID不为空");
             }
-            if (activitySolidAwardYardVo.getActivitySolidYard() == null){
-                throw  new ServiceException("兑换码不为空");
+            if (activitySolidAwardYardVo.getActivitySolidYard() == null) {
+                throw new ServiceException("兑换码不为空");
             }
             activitySolidAwardYardVo.setId(UUIDUtils.generateUuid());
             activitySolidAwardYardVo.setType(ActivitySolidAwardYardTypes.ZERO.getType());
 
-        }else {
-            if (StringUtils.isBlank(activitySolidAwardYardVo.getUpdatedBy())){
-                throw new  ServiceException("beizhu修改人");
+        } else {
+            if (StringUtils.isBlank(activitySolidAwardYardVo.getUpdatedBy())) {
+                throw new ServiceException("beizhu修改人");
             }
-            if (activitySolidAwardYard.getType().equals(ActivitySolidAwardYardTypes.ONE.getType())){
-                throw new  ServiceException("兑奖码状态不正确");
+            if (activitySolidAwardYard.getType().equals(ActivitySolidAwardYardTypes.ONE.getType())) {
+                throw new ServiceException("兑奖码状态不正确");
             }
             activitySolidAwardYardVo.setType(ActivitySolidAwardYardTypes.ONE.getType());
         }
-
     }
-
 }
