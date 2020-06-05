3bee9ed07ccc97d730a2b37ece9f867db5dc2e6c
zwq <zhangwanqi@sdxnetcafe.com>
2020-06-05 18:00:16 +0800

    compareTo比较价格

--- ly_item/ly_item_service/src/test/java/com/leyou/item/biz/demo03.java
+++ ly_item/ly_item_service/src/test/java/com/leyou/item/biz/demo03.java
@@ -2,6 +2,7 @@ package com.leyou.item.biz;
 
 import com.leyou.common.utils.JsonUtils;
 
+import java.math.BigDecimal;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -10,7 +11,7 @@ import java.util.List;
 
 public class demo03 {
 
-    public static void main(String[] args) {
+    public static void main1(String[] args) {
         String beginTime = "2018-07-30 14:42:32";
         String endTime = "2018-07-29 12:26:32";
 
@@ -26,6 +27,35 @@ public class demo03 {
 
         } catch (ParseException e) {
             e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+
+
+        BigDecimal a = new BigDecimal(121);
+        BigDecimal b = new BigDecimal(111);
+
+        //使用 compare To方法比较
+        //注意：a、b均不能为null，否则会报空指针
+        if (a.compareTo(b) == -1) {
+            System.out.println("a小于b");
+        }
+
+        if (a.compareTo(b) == 0) {
+            System.out.println("a等于b");
+        }
+
+        if (a.compareTo(b) == 1) {
+            System.out.println("a大于b");
+        }
+
+        if (a.compareTo(b) > -1) {
+            System.out.println("a大于等于b");
+        }
+
+        if (a.compareTo(b) < 1) {
+            System.out.println("a小于等于b");
         }
     }
 }
