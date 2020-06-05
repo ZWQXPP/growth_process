1219ecd0093d3733d3bc12102bf1097805114479
zwq <zhangwanqi@sdxnetcafe.com>
2020-06-05 11:58:27 +0800

    获取随机数

--- ly_item/ly_item_service/src/test/java/com/leyou/item/biz/demo01.java
+++ ly_item/ly_item_service/src/test/java/com/leyou/item/biz/demo01.java
@@ -7,13 +7,41 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
+import java.util.Random;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class demo01 {
     @Test
-    public void demo01(){
-
-        System.out.println("==========");
+    public void demo01() {
+        Random r = new Random(1);
+            int ran1 = r.nextInt(10);
+            System.out.println(ran1);
     }
+
+    @Test
+    public void demo02(){
+        for (int i = 0; i < 5; i++) {
+            int max=10,min=1;
+            int ran2 = (int) (Math.random()*(max-min)+min);
+            System.out.println(ran2);
+        }
+    }
+@Test
+    public void demo03(){
+            int max=100,min=1;
+            long randomNum = System.currentTimeMillis();
+            int ran3 = (int) (randomNum%(max-min)+min);
+            System.out.println(ran3);
+        }
+
 }
+
+
+
+
+
+
+
+
+
